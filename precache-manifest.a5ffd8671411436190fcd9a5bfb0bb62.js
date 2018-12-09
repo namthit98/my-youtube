9284c1f129f45d7dffecc79a16f30a19ef9af2fd
NamHandsome <namthit98@gmail.com>
2018-12-09 21:38:53 +0700

    Updates

---
+++ precache-manifest.a5ffd8671411436190fcd9a5bfb0bb62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2feeef14e06af9ad9409",
+    "url": "/my-youtube/static/js/runtime~main.2feeef14.js"
+  },
+  {
+    "revision": "fa4ac8d9d69b5b5eba61",
+    "url": "/my-youtube/static/js/main.fa4ac8d9.chunk.js"
+  },
+  {
+    "revision": "d1a4e5ca5410e2fb96d6",
+    "url": "/my-youtube/static/js/1.d1a4e5ca.chunk.js"
+  },
+  {
+    "revision": "fa4ac8d9d69b5b5eba61",
+    "url": "/my-youtube/static/css/main.6db3d589.chunk.css"
+  },
+  {
+    "revision": "986bdce103252d3b7b668814b713eb6d",
+    "url": "/my-youtube/index.html"
+  }
+];
